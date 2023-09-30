826f80957a9e28d27ca2241825db7bffecf284e1
mfubilla <marce.ubi@hotmail.com>
2023-09-30 16:09:52 -0300

    se agregega una prediccion

---
+++ prediction.py
@@ -0,0 +1,16 @@
+# Supongamos que queremos predecir para un estudiante con una calificación de 4 en la primera parte y 8 en la segunda parte.
+nueva_data = {
+    'Nota_Part1': [4],
+    'Nota_Part2': [8],
+}
+
+# Crear un DataFrame con la nueva data
+nuevo_df = pd.DataFrame(nueva_data)
+
+# Realizar la predicción utilizando el modelo XGBoost entrenado
+probabilidad_prediccion = modelo.predict_proba(nuevo_df)
+
+# La probabilidad de obtener una calificación de 6 o más en el examen final se encuentra en probabilidad_prediccion[0][1]
+probabilidad = probabilidad_prediccion[0][1] * 100  # Multiplicar por 100 para expresarla en porcentaje
+
+print(f"La probabilidad de obtener una calificación de 6 o más en el examen final es del {probabilidad:.2f}%")
